ce03f96b2891d07e81a6a25b1bf92306cb6a55e2
Hugo071 <99701511+Hugo071@users.noreply.github.com>
2024-12-04 18:28:28 -0600

    env

--- registro_facial.py
+++ registro_facial.py
@@ -42,8 +42,7 @@ load_dotenv()
 def conectar_mongobb(MONGO_HOST = "localhost", MONGO_PORT = "27017", MONGO_DB = "Ejemplo", MONGO_TIMEOUT = 1000):
     try:
         #MONGO_URI = "mongodb://localhost:27017"
-        MONGO_URI = os.getenv("MONGO_URI")
-        MONGO_CLIENT = MongoClient(MONGO_URI)
+        MONGO_CLIENT = MongoClient(os.getenv("MONGO_URI"))
         try:
             return MONGO_CLIENT
         except OperationFailure as error:
